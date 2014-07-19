f7a36bb1f9aec2536c189e4a0b0426a610f2f464
Pawel Suminski <p.suminski@gmail.com>
2014-07-20 01:57:22 +0200

    ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,17 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [ :create]
   def create
   	@comment = Comment.new(comment_params.merge(:user_id=>current_user.id.to_i))
-  	if @comment.save
-     redirect_to article_path(@comment.article), notice: 'Komentarz dodany'
-    else
-      redirect_to article_path(@comment.article), alert: 'Blad przy dodawaniu komentarza'
+  	#if @comment.save
+    # redirect_to article_path(@comment.article), notice: 'Komentarz dodany'
+    #else
+     # redirect_to article_path(@comment.article), alert: 'Blad przy dodawaniu komentarza'
+    #end
+    @comment.save
+    #ajax nie dziala, mimo ze wszystko jest tak samo jak innym moim projekcie, w ktorym ajax dziala: https://github.com/reynevan/rails_ajax
+    respond_to do |format|
+      format.html {redirect_to article_path(@comment.article) }
+      format.js {}
     end
-    
   end
 
 
