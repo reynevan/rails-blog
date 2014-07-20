0dd23e038ed671bc783c8f8e8426c4f70ce0f02c
reynevan <p.suminski@gmail.com>
2014-07-20 08:21:09 +0200

    tabs to spaces convert

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function(){
-	var hidden = 0;
-	$('body').on('mousewheel', function(event) {
-		if ( ($(document).scrollTop() == 0) && hidden ){
-			  $('#logo').slideDown('fast');
+    var hidden = 0;
+    $('body').on('mousewheel', function(event) {
+        if ( ($(document).scrollTop() == 0) && hidden ){
+              $('#logo').slideDown('fast');
         hidden = 0;
-				}
-		if ((!hidden) && ($(document).scrollTop() != 0)){
-			$('#logo').slideUp('fast');
-			event.preventDefault();
-			event.stopPropagation();
-			hidden=1;
-			return;
-		}
-	});
-
-
+                }
+        if ((!hidden) && ($(document).scrollTop() != 0)){
+            $('#logo').slideUp('fast');
+            event.preventDefault();
+            event.stopPropagation();
+            hidden=1;
+            return;
+        }
+    });
 })
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [ :create]
   def create
-  	@comment = Comment.new(comment_params.merge(:user_id=>current_user.id.to_i))
-  	#if @comment.save
+    @comment = Comment.new(comment_params.merge(:user_id=>current_user.id.to_i))
+    #if @comment.save
     # redirect_to article_path(@comment.article), notice: 'Komentarz dodany'
     #else
      # redirect_to article_path(@comment.article), alert: 'Blad przy dodawaniu komentarza'
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
 
 
   private
-	def comment_params
+  def comment_params
       params.require(:comment).permit(:article_id, :content, :user_id)
     end
 end
